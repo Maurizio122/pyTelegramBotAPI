1630888689330bc6d3fafe91cd62664204e8bdc7
Maurizio122 <79162637+Maurizio122@users.noreply.github.com>
2021-02-16 18:48:40 +0100

    Update and rename examples/echo_bot.py to esempi/echo_bot.py

--- esempi/echo_bot.py
+++ esempi/echo_bot.py
@@ -5,7 +5,7 @@
 
 import telebot
 
-API_TOKEN = '<api_token>'
+API_TOKEN = '<1640195297:AAG71yRi5eZqjz7h1atvcpi3jLNI1Tka9zA>'
 
 bot = telebot.TeleBot(API_TOKEN)
 
@@ -14,15 +14,19 @@ bot = telebot.TeleBot(API_TOKEN)
 @bot.message_handler(commands=['help', 'start'])
 def send_welcome(message):
     bot.reply_to(message, """\
-Hi there, I am EchoBot.
-I am here to echo your kind words back to you. Just say anything nice and I'll say the exact same thing to you!\
+Ciao, questo bot è usato solo per scambi seri, no perdite tempo!\
 """)
 
 
 # Handle all other messages with content_type 'text' (content_types defaults to ['text'])
 @bot.message_handler(func=lambda message: True)
 def echo_message(message):
-    bot.reply_to(message, message.text)
+
+else : 
+
+    bot.reply_to(message, 'Se vuoi scambiare contatta @AMPISCORA')
+
+
 
 
 bot.polling()
